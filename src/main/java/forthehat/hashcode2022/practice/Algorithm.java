b637c7921784c126f8094546a66fba16e2295b6d
Mario Lehnerer <mario.lehnerer2001@gmail.com>
2022-02-19 15:10:06 +0100

    hat

--- src/main/java/forthehat/hashcode2022/practice/Algorithm.java
+++ src/main/java/forthehat/hashcode2022/practice/Algorithm.java
@@ -5,9 +5,32 @@ import java.util.List;
 
 public class Algorithm {
     private List<String> clients = new ArrayList<String>();
+    private List<String> ingredients = new ArrayList<String>();
+    private List<String> currentSolution = new ArrayList<String>();
 
     Algorithm(List<String> clients){
         this.clients = clients;
+    }
+
+    public List<String> initialSolution() {
+        for(String ingredient: ingredients){
+            if (ingredient.isDisliked == 0 || ingredient.isLiked() > ingredient.isDisliked() * 5){
+                currentSolution.add(ingredient);
+            }
+        }
+        return currentSolution;
+    }
+
+    public List<String> evolutionarySolution() {
+
+    }
+
+    public List<String> getChild(){
+
+    }
+
+    public List<String> getCurrentlyUnusedIngredients(){
+
     }
 
 }
