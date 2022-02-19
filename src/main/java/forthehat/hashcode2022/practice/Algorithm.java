c057641b358e33eff9cc4cdaa29917802525ac7d
Mario Lehnerer <mario.lehnerer2001@gmail.com>
2022-02-19 14:58:11 +0100

    hat

---
+++ src/main/java/forthehat/hashcode2022/practice/Algorithm.java
@@ -0,0 +1,13 @@
+package forthehat.hashcode2022.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algorithm {
+    private List<String> clients = new ArrayList<String>();
+
+    Algorithm(List<String> clients){
+        this.clients = clients;
+    }
+
+}
