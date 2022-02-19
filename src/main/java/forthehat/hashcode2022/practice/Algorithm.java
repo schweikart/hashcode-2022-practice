347bb8c49d32fd268066c02eadc0d03acd736842
Mario Lehnerer <mario.lehnerer2001@gmail.com>
2022-02-19 15:35:53 +0100

    hat

--- src/main/java/forthehat/hashcode2022/practice/Algorithm.java
+++ src/main/java/forthehat/hashcode2022/practice/Algorithm.java
@@ -11,33 +11,61 @@ public class Algorithm {
     private static final int LIKED_TO_DISLIKED_RATIO = 5;
     private static final int NEW_INGREDIENT_PER_CHILD = 5;
 
-    Algorithm(List<String> clients){
+    Algorithm(List<String> clients) {
         this.clients = clients;
     }
 
     public List<String> initialSolution() {
-        for(String ingredient: ingredients){
-            if (ingredient.isDisliked == 0 || ingredient.isLiked() > ingredient.isDisliked() * LIKED_TO_DISLIKED_RATIO){
+        for (String ingredient : ingredients) {
+            if (ingredient.isDisliked == 0 || ingredient.isLiked() > ingredient.isDisliked() * LIKED_TO_DISLIKED_RATIO) {
                 currentSolution.add(ingredient);
             }
         }
         return currentSolution;
     }
 
-    public List<String> evolutionarySolution() {
-
+    public List<String> getBetterSolution() {
+        for (int i =0; i <1000; i++){
+            getFavoritChild();
+        }
+        return this.currentSolution;
     }
 
-    public List<String> getChild(){
-
+    public List<String> getFavoritChild() {
+        List<List<String>> childs = null;
+        for (int i = 0; i < 100; i++) {
+            childs.add(this.getChild());
+        }
+        int bestSolution = 0;
+        List<String> favoritChild = null;
+        for(List<String> child : childs){
+            int childSolution = Scoring.scoring(child, clients);
+            if(childSolution > bestSolution){
+                bestSolution = childSolution;
+                favoritChild = child;
+            }
+        }
+        System.out.println(bestSolution);
+        return favoritChild;
     }
 
-    public List<String> getCurrentlyUnusedIngredients(){
+    public List<String> getChild() {
+        List<String> unUsedIngredients = this.getCurrentlyUnusedIngredients();
+        for (int i = 0; i < Math.min(Math.random() * NEW_INGREDIENT_PER_CHILD, unUsedIngredients.size()); i++) {
+            String ingredient = unUsedIngredients.get((int) Math.random() * unUsedIngredients.size());
+            currentSolution.add(unUsedIngredients.get((int) Math.random() * unUsedIngredients.size()));
+            unUsedIngredients.remove(ingredient);
+        }
+        for (int i = 0; i < Math.min(Math.random() * NEW_INGREDIENT_PER_CHILD, currentSolution.size()); i++) {
+            currentSolution.remove(currentSolution.get((int) Math.random() * currentSolution.size()));
+        }
+    }
+
+    public List<String> getCurrentlyUnusedIngredients() {
         unusedIngredients = ingredients;
-        for(String usedIngredient : currentSolution) {
+        for (String usedIngredient : currentSolution) {
             unusedIngredients.remove(usedIngredient);
         }
         return unusedIngredients;
     }
-
 }
